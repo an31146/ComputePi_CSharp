3bc98b981189e67956a75f6abf04eff38cc98c28
an31146 <richard.hammond@att.net>
2019-05-20 23:29:13 +0100

    Added a couple of alternative square root functions

--- IntegerPi/Program.cs
+++ IntegerPi/Program.cs
@@ -8,15 +8,15 @@ using static System.Console;
 
 namespace IntegerPi
 {
-#pragma warning disable IDE1006
+#pragma warning disable IDE1006,IDE0049
     class Program
     {
-        const uint STEPS = 10000000;
+        const uint STEPS = 1000000;
         const uint DIGITS = 2000;
 
-        static public BigInteger SquareRoot(BigInteger n)
+        public static BigInteger SquareRoot(BigInteger n)
         {
-            BigInteger d = BigInteger.One, q, _d;
+            BigInteger div = BigInteger.One, quotient, _doubleOfQuotient, _remainder;
             bool bBreakExpr = true; 
             Stopwatch sw = new Stopwatch();
 
@@ -24,13 +24,17 @@ namespace IntegerPi
             sw.Start();
             do
             {
-                q = n / d + d;
-                _d = q;
-                q >>= 1;                                // _q = q / 2;
-                d = q;                                  // _d = 2*d
+                quotient = n / div + div;
+                _doubleOfQuotient = quotient;                   // _doubleOfquotient;       preserve double of quotient before halving
+                quotient >>= 1;                                 // right-shfit quotient;
+                div = quotient;                                 // assign next divisor
+                bBreakExpr = quotient * quotient > n;           // Can't figure out a test expression without using multiplication here :-(
+                
+                //bBreakExpr = quotient < _div;
+                //_remainder = _doubleOfQuotient - div - div;     // 
+                //bBreakExpr = _remainder.IsOne || _remainder.IsZero;       // test if remainder is either 1 or 0
+                //bBreakExpr = quotient.Equals(div) || quotient.Equals(_doubleOfQuotient);
 
-                bBreakExpr = q * q > n;                 // Can't figure out a test expression without using multiplication here :-(
-                //bBreakExpr = 2 * q < _d; // && q * 2 != d;
             } while (bBreakExpr);
             sw.Stop();
 #if DEBUG
@@ -40,10 +44,10 @@ namespace IntegerPi
             else
                 strElapsed = String.Format("{0:F1} s", sw.Elapsed.TotalSeconds);
 
-            WriteLine("\nSquareRoot({0})\nElapsed time: {1}\n", n, strElapsed);
+            WriteLine($"\nSquareRoot:\n{quotient}\nElapsed time: {strElapsed}\n");
 #endif
-            return q;
-        }
+            return quotient;
+        }   // SquareRoot
 
         public static BigInteger Sqrt(BigInteger x)
         {
@@ -73,10 +77,86 @@ namespace IntegerPi
             else
                 strElapsed = String.Format("{0:F1} s", sw.Elapsed.TotalSeconds);
 
-            WriteLine("\nSquareRoot({0})\nElapsed time: {1}\n", y, strElapsed);
+            WriteLine($"\nSquareRoot:{y}\nElapsed time: {strElapsed}\n");
 #endif
             return y;
-        }
+        }   // Sqrt
+
+        public static BigInteger SquareRootFloor(BigInteger x)
+        {
+            if (x.CompareTo(BigInteger.Zero) < 0) {
+                throw new ArgumentException("Negative argument.");
+            }
+            
+            // square roots of 0 and 1 are trivial and
+            // y == 0 will cause a divide-by-zero exception
+            if (x.Equals(BigInteger.Zero) || x.Equals(BigInteger.One)) {
+                return x;
+            }
+
+            BigInteger TWO = new BigInteger(2L);
+            BigInteger y;
+            Stopwatch sw = new Stopwatch();
+
+            // starting with y = x / 2 avoids magnitude issues with x squared
+            sw.Start();
+            for (y = BigInteger.Divide(x, TWO);
+                 y.CompareTo(BigInteger.Divide(x, y)) > 0;
+                 y = BigInteger.Divide(BigInteger.Add(BigInteger.Divide(x, y), y), TWO));
+            sw.Stop();
+#if DEBUG
+            string strElapsed;
+            if (sw.ElapsedMilliseconds <= 1000)
+                strElapsed = String.Format("{0} ms", sw.ElapsedMilliseconds);
+            else
+                strElapsed = String.Format("{0:F1} s", sw.Elapsed.TotalSeconds);
+
+            WriteLine($"\nSquareRootFloor:\n{y})\nElapsed time: {strElapsed}\n");
+#endif
+            return y;
+        }   // SquareRootFloor
+
+        public static BigInteger SquareRootCeil(BigInteger x)
+        {
+            if (x.CompareTo(BigInteger.Zero) < 0) {
+                throw new ArgumentException("Negative argument.");
+            }
+            
+            // square roots of 0 and 1 are trivial and
+            // y == 0 will cause a divide-by-zero exception
+            if (x == BigInteger.Zero || x == BigInteger.One) {
+                return x;
+            }
+
+            BigInteger TWO = new BigInteger(2L);
+            BigInteger y;
+            Stopwatch sw = new Stopwatch();
+
+            // starting with y = x / 2 avoids magnitude issues with x squared
+            sw.Start();
+            for (y = BigInteger.Divide(x, TWO);
+                 y.CompareTo(BigInteger.Divide(x, y)) > 0;
+                 y = BigInteger.Divide(BigInteger.Add(BigInteger.Divide(x, y), y), TWO));
+            sw.Stop();
+#if DEBUG
+            string strElapsed;
+            if (sw.ElapsedMilliseconds <= 1000)
+                strElapsed = String.Format("{0} ms", sw.ElapsedMilliseconds);
+            else
+                strElapsed = String.Format("{0:F1} s", sw.Elapsed.TotalSeconds);
+
+            WriteLine($"\nSquareRootCeil:\n{y})\nElapsed time: {strElapsed}\n");
+#endif
+
+            if (x.CompareTo(BigInteger.Multiply(y, y)) == 0)
+            {
+                return y;
+            }
+            else
+            {
+                return BigInteger.Add(y, BigInteger.One);
+            }
+        }   // SquareRootCeil
 
         static double zeta_of_two_double()
         {
@@ -121,7 +201,6 @@ namespace IntegerPi
             return sum;
         }
 
-
         static BigInteger normalize(uint n, uint digits)
         {
             BigInteger ret_val = new BigInteger(n);
@@ -134,7 +213,7 @@ namespace IntegerPi
                     exponent *= 10;
 
                 exponent *= exponent;
-                i /= 2;
+                i >>= 1;
             }
             ret_val *= exponent;
 
@@ -149,6 +228,8 @@ namespace IntegerPi
             BigInteger sum = unity;
             unity *= unity;
 
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
             for (uint i = 2; i < STEPS; i++)
             {
                 BigInteger b = i * exponent;
@@ -156,7 +237,16 @@ namespace IntegerPi
                 sum += unity / b;
                 //Write("{0}\r", i);
             }
+            sw.Start();
+#if DEBUG
+            string strElapsed;
+            if (sw.ElapsedMilliseconds <= 1000)
+                strElapsed = String.Format("{0} ms", sw.ElapsedMilliseconds);
+            else
+                strElapsed = String.Format("{0:F1} s", sw.Elapsed.TotalSeconds);
 
+            WriteLine($"\nzeta_of_two_bigint:\n{sum})\nElapsed time: {strElapsed}\n");
+#endif
             WriteLine();
             //WriteLine("{0}", unity);
             return sum;
@@ -170,13 +260,15 @@ namespace IntegerPi
             double pi_to_the_fourth_over_ninety = zeta_of_four_double();
             WriteLine("pi⁴/90: {0}\n\n(pi⁴/90)^¼: {1}\n\n", pi_to_the_fourth_over_ninety, Math.Pow(pi_to_the_fourth_over_ninety * 90.0d, 0.25d));
 
-            BigInteger TWO = normalize(2, DIGITS);
+            BigInteger TWO = normalize(3, DIGITS);
             WriteLine("SquareRoot({0}) =\n{1}\n", TWO, SquareRoot(TWO));
             WriteLine("Sqrt({0}) =\n{1}\n", TWO, Sqrt(TWO));
+            WriteLine("SquareRootFloor({0}) =\n{1}\n", TWO, SquareRootFloor(TWO));
+            WriteLine("SquareRootCeil({0}) =\n{1}\n", TWO, SquareRootCeil(TWO));
 
             BigInteger BigInt_pi_squared_over_six = zeta_of_two_bigint();
             BigInteger BigInt_pi = SquareRoot(BigInt_pi_squared_over_six * 6);
-            WriteLine("BigInt_pi²/6: {0}\n\n√(BigInt_pi²/6): {1}\n\n", BigInt_pi_squared_over_six.ToString(), BigInt_pi.ToString());
+            WriteLine("BigInt_pi²/6: {0}\n\n√(BigInt_pi²/6): {1}\n\n", BigInt_pi_squared_over_six, BigInt_pi);
 
             Write("Press Enter: ");
             ReadLine();
