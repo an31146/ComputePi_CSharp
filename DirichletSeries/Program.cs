29bd81a0d3f70213e605ca7ca1a90996740334ca
an31146 <richard.hammond@att.net>
2019-05-20 13:24:44 +0100

    Add project files.

---
+++ DirichletSeries/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Numerics;
+using static System.Math;
+using static System.Console;
+
+namespace DirichletSeries
+{
+    static class Program
+    {
+        /// <summary>
+        /// Calculates the converged point for a Dirichlet series expansion.
+        /// </summary>
+        /// <param name="t">imaginary part of s. The first zero is at 14.134725</param>
+        /// <param name="numberOfTerms">Use a higher number to find more accurate convergence.</param>
+        /// <returns></returns>
+        /// 
+        static Complex CalcZetaZero(this double t, int numberOfTerms)
+        {
+            var range = Enumerable.Range(1, numberOfTerms);
+            var zetaZero = Complex.Zero;
+
+            foreach (int n in range)
+            {
+                var direction = n % 2 == 0 ? PI : 0;
+                var newTerm = Complex.Exp(new Complex(-Log(n) * .5, -Log(n) * t + direction));
+                zetaZero += newTerm;
+            }
+
+            return zetaZero;
+        }
+
+        static void Main(string[] args)
+        {
+            for (int terms = 100000; terms <= 1000000000; terms *= 10)
+                WriteLine("{0}", CalcZetaZero(14.134725141734, terms));
+            
+            //for (double t=14.134725; t<14.134726; t+=1E-8)
+            //    Console.WriteLine("{0:0.0000000000} : {1}", t, CalcZetaZero(t, 10000000).Magnitude);
+
+            Write("Press Enter: ");
+            ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿//--------------------------------------------------------------------------
+// 
+//  Copyright (c) Microsoft Corporation.  All rights reserved. 
+// 
+//  File: Program.cs
+//
+//--------------------------------------------------------------------------
+
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComputePi
+{
+    class Program
+    {
+        const int num_steps = 1000000000;
+
+        /// <summary>Main method to time various implementations of computing PI.</summary>
+        static void Main(string[] args)
+        {
+            ParallelForCancellation p = new ParallelForCancellation();
+            //p.CancelDemo();
+
+            Time("SerialLinqPi()", () => SerialLinqPi());
+            Time("ParallelLinqPi()", () => ParallelLinqPi());
+            Time("SerialPi()", () => SerialPi());
+            Time("ParallelPi()", () => ParallelPi());
+            Time("ParallelPartitionerPi()", () => ParallelPartitionerPi());
+
+            Console.WriteLine("---- Press Enter ----");
+            Console.ReadLine();
+        }
+
+        /// <summary>Times the execution of a function and outputs both the elapsed time and the function's result.</summary>
+        static void Time<T>(String strFuncName, Func<T> work)
+        {
+            var sw = Stopwatch.StartNew();
+            var result = work();
+            Console.WriteLine("--- {0} ---", strFuncName);
+            Console.WriteLine(sw.Elapsed + ": " + result + "\n");
+        }
+
+        /// <summary>Estimates the value of PI using a LINQ-based implementation.</summary>
+        static double SerialLinqPi()
+        {
+            double step = 1.0 / (double)num_steps;
+            return (from i in Enumerable.Range(0, num_steps)
+                    let x = (i + 0.5) * step
+                    select 4.0 / (1.0 + x * x)).Sum() * step;
+        }
+
+        /// <summary>Estimates the value of PI using a PLINQ-based implementation.</summary>
+        static double ParallelLinqPi()
+        {
+            double step = 1.0 / (double)num_steps;
+            return (from i in ParallelEnumerable.Range(0, num_steps)
+                    let x = (i + 0.5) * step
+                    select 4.0 / (1.0 + x * x)).Sum() * step;
+        }
+
+        /// <summary>Estimates the value of PI using a for loop.</summary>
+        static double SerialPi()
+        {
+            double sum = 0.0;
+            double step = 1.0 / (double)num_steps;
+            for (int i = 0; i < num_steps; i++)
+            {
+                double x = (i + 0.5) * step;
+                sum = sum + 4.0 / (1.0 + x * x);
+            }
+            return step * sum;
+        }
+
+        /// <summary>Estimates the value of PI using a Parallel.For.</summary>
+        static double ParallelPi()
+        {
+            double sum = 0.0;
+            double step = 1.0 / (double)num_steps;
+            object monitor = new object();
+            Parallel.For(0, num_steps, () => 0.0, (i, state, local) =>
+            {
+                double x = (i + 0.5) * step;
+                return local + 4.0 / (1.0 + x * x);
+            }, local => { lock (monitor) sum += local; });
+            return step * sum;
+        }
+
+        /// <summary>Estimates the value of PI using a Parallel.ForEach and a range partitioner.</summary>
+        static double ParallelPartitionerPi()
+        {
+            double sum = 0.0;
+            double step = 1.0 / (double)num_steps;
+            object monitor = new object();
+            Parallel.ForEach(Partitioner.Create(0, num_steps), () => 0.0, (range, state, local) =>
+            {
+                for (int i = range.Item1; i < range.Item2; i++)
+                {
+                    double x = (i + 0.5) * step;
+                    local += 4.0 / (1.0 + x * x);
+                }
+                return local;
+            }, local => { lock (monitor) sum += local; });
+            return step * sum;
+        }
+    }
+}
